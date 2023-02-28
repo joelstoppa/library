84d79a19d56c91935cb81ba5367063313486fc51
joelstoppa <joel.stoppa@gmail.com>
2023-02-28 17:49:45 +0100

    Add function to display array content

--- app.js
+++ app.js
@@ -10,3 +10,17 @@ function Book(title, author, pages, read) {
 function addBookToLibrary(Book) {
   myLibrary.push(Book);
 }
+
+const theHobbit = new Book("The Hobbit", "Tolkien", 300, "read");
+addBookToLibrary(theHobbit);
+
+const harryPotter = new Book("Harry Potter", "J.K. Rowling", 450, "not read");
+addBookToLibrary(harryPotter);
+
+const container = document.querySelector("main");
+
+myLibrary.forEach((book) => {
+  let card = document.createElement("div");
+  container.append(card);
+  card.innerHTML = `<p>Title: ${book.title}</p> <p>Author: ${book.author}</p> <p>Pages: ${book.pages}</p> <p>Read: ${book.read}</p>`;
+});
